2790e15bd403060227189293efd0b1d6cf925a19
CppCommons <cppcommons@gmail.com>
2017-09-22 10:39:23 +0900

    .

---
+++ app-client/src/main/java/Client.java
@@ -0,0 +1,27 @@
+
+import lombok.extern.slf4j.Slf4j;
+import lombok.Data;
+import lombok.val;
+
+@Slf4j
+public class Client {
+
+    public static String GREETING = "Hello World";
+
+    public static void main(String[] args) {
+        System.out.println("main");
+        log.info("main8");
+        log.info("{}", Libs01.add2(22, 33));
+        val data = new MyData("A");
+        log.info(data.toString());
+        log.info("data={}", data);
+    }
+
+    @Data
+    public static class MyData {
+
+        private final String required;
+        private int number;
+    }
+
+}
